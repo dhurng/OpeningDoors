f8c02d347bd78de87ee9d1d2b7d160180ce42ae2
jonjeng <jonjeng2@gmail.com>
2017-01-04 11:10:58 -0800

    Add files via upload

--- email_script.php
+++ email_script.php
@@ -1,18 +1,42 @@
 <?php
-    
-$from=$_POST['name'];
-$email=$_POST['email'];
-$subject=$_POST['subject'];
-$message=$_POST['message'];
-$resume=$_POST['resume'];
+
+$to = 'support@openingdoorsfoundation.org';
+$name = $_POST['name'];
+$email = $_REQUEST['email'];
+$subject = $_REQUEST['subject'];
+$message = $_REQUEST['message'];
 
 
-//$to = yangyangshi2010@gmail.com;
+$resume = $_POST['resume'];
 
-$to = dhurng@gmail.com
-    
-mail($to, $subject, $message, "From:".$email);
 
-print "Your message has been sent: </br>$name</br>$subject</br>$message</br>$resume</p>";
+print "Your message has been sent: support@openingdoorsfoundation.org
+    </br>
+    From: $name at $email</br>
+    About: $subject</br>
+            $message</br>
+            $resume</br>
+    Thank you!";
 
+$message = $message . " from: " . $email;
+
+
+if (mail($to, $subject, $message)) {
+    header("Location: index.php");
+} else {
+    echo "Sorry there seems to be an error.";
+}
+
+//  mail($)
+//  //send email
+//  mail($admin_email, "$subject", $comment, "From:" . $email);
+//
+//  //Email response
+//  echo "Thank you for contacting us!";
+//
+//print "Your message has been sent:        </br>$name</br>$subject</br>$message</br>";
+//      //redirect later
+//  }
+//
+//if "email" variable is not filled out, display the form
 ?>
